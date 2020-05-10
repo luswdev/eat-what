3248283b748fc563da6174cde0e4a98dc7092eb7
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-05-10 21:10:40 +0800

    add random picker

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready( function () {
     let food;
     $.getJSON("/data/food.json", function (json) {
@@ -5,6 +7,32 @@ $(document).ready( function () {
     });
 
     $(".choose-btn").on("click", function () {
-        $(".main-inner").append("test");
+        var which_index = $("#which-picker input").index($("#which-picker input:checked"));
+
+        /* get right restaurants pool */
+        var pool;
+        if (which_index == 0) {
+            pool = food.brunch;
+        } else {
+            pool = food.dinner;
+        }
+
+        /* create a random index number */
+        var pool_length = pool.length;
+        var pool_index = Math.randomInt(pool_length)
+
+        /* get current reataurant */
+        pool = shuffle(pool);
+        var rastaurant = pool[pool_index];
+        
+
+        $(".main-inner").append(rastaurant);
     });
 });
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
