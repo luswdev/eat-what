7f41f50ada0e24be52538ccb4f5d97f5550e18ce
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-05-10 23:07:20 +0800

    rm CNAME

--- _asserts/js/main.js
+++ _asserts/js/main.js
@@ -6,7 +6,7 @@ $(document).ready( function () {
 
     /* get data from JSON */
     let food;
-    $.getJSON("/_data/food.json", function (json) {
+    $.getJSON("/data/food.json", function (json) {
         food = json;
     });
 
