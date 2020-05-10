3bb5619ba55f21a968bcd40aa3b5c88a05db5e89
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-05-10 20:51:42 +0800

    testing button

--- asserts/js/main.js
+++ asserts/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready( function () {
-    
+    let food;
+    $.getJSON("/data/food.json", function (json) {
+        food = json;
+    });
+
+    $(".choose-btn").on("click", function () {
+        $(".main-inner").append("test");
+    });
 });
